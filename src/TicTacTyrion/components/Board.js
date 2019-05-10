bfc0f4ed1c8a6db3347916d04712c753db5a4839
Eric Mauldin <emauldin84@gmail.com>
2019-05-10 11:02:36 -0400

    changes board to match player reaction

--- src/TicTacTyrion/components/Board.js
+++ src/TicTacTyrion/components/Board.js
@@ -1,17 +1,36 @@
 import React from 'react'
 
-export default function Board({board, clickHandler}) {
+export default function Board({board, clickHandler, checkWinner}) {
     return (
         <div className='ticTacToeBoard'>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.A1}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.A2}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.A3}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.B1}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.B2}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.B3}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.C1}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.C2}</div>
-            <div className='ticTacToeSquare' onClick={clickHandler}>{board.C3}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('A1')
+                checkWinner()
+            }}>{board.A1}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('B1')
+            }}>{board.B1}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('C1')
+            }}>{board.C1}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('A2')
+            }}>{board.A2}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('B2')
+            }}>{board.B2}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('C2')
+            }}>{board.C2}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('A3')
+            }}>{board.A3}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('B3')
+            }}>{board.B3}</div>
+            <div className='ticTacToeSquare' onClick={()  => {
+                clickHandler('C3')
+            }}>{board.C3}</div>
         </div>
     )
 }
--- src/TicTacTyrion/components/TicTacTyrion.js
+++ src/TicTacTyrion/components/TicTacTyrion.js
@@ -8,19 +8,19 @@ export default class TicTacTyrion extends Component {
         this.state = {
             playerOneName: 'Player One', // player one is always 'X'
             playerTwoName: 'Player Two', // player two is always 'O'
-            isPlayerOneTurn: true,
+            isPlayerOneTurn: false,
             // Letters for columns, numbers for rows
-            // There will be eight arrays containing the board values ('X', 'O' or null) that will be checked after each turn.  
+            // There will be eight arrays containing the board values ('X', 'O' or null) that will be checked after each turn. 
             board: {
-                'A1': 'X',
-                'A2': 'O',
-                'A3': 'X',
-                'B1': 'O',
-                'B2': 'X',
-                'B3': 'O',
-                'C1': 'X',
-                'C2': 'O',
-                'C3': 'X',
+                'A1': 'A1',
+                'A2': 'A2',
+                'A3': 'A3',
+                'B1': 'B1',
+                'B2': 'B2',
+                'B3': 'B3',
+                'C1': 'C1',
+                'C2': 'C2',
+                'C3': 'C3',
             },
             turnCounter: 0,
             winner: null, // after turnCounter = 9 check array, then result in 'draw' if no arrays === true. Otherwise, 'player one' or 'player two'
@@ -33,7 +33,7 @@ export default class TicTacTyrion extends Component {
         <div>
             <h1>TicTacTyrion</h1>
             {/* <PlayerOne /> */}
-            <Board board={this.state.board} clickHandler={this.setCell}/>
+            <Board board={this.state.board} clickHandler={this._setCell} checkWinner={this._checkForWinner}/>
             {/* <PlayerTwo /> */}
             {/* Result to display start button or display who won (or lion-scratch) with a play again at end of game */}
             {/* <Result />  */}
@@ -50,15 +50,35 @@ export default class TicTacTyrion extends Component {
 
         }
 
-        _setCell = () => {
+        _setCell = (cell) => {
             // sets value of cell depending on the player turn
+            const tempBoard = {...this.state.board}
+            if (this.state.isPlayerOneTurn) {
+                tempBoard[cell] = 'X'
+            } else {
+                tempBoard[cell] = 'O'
+            }
+            this.setState({
+                board: tempBoard,
+                
+            })
+            console.log(tempBoard)
         
         }
 
         _checkForWinner = () => {
             // set turn counter
+                
             // set player turn
             // set winner
+            this.setState({
+
+                turnCounter: ,
+                isPlayerOneTurn: !isPlayerOneTurn,
+                winner: ,
+
+
+            })
         }
 
         _newGame = () => {
