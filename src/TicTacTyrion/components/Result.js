09d058ec1b07c5653482976b8ff109e4e09c5269
Eric Mauldin <emauldin84@gmail.com>
2019-05-11 15:43:39 -0400

    pulses 'new game' button if winner or tie

--- src/TicTacTyrion/components/Result.js
+++ src/TicTacTyrion/components/Result.js
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Result({winner, clickHandler}) {
     return (
         <div>
-            <h1>{winner} {((winner === null) || (winner === "Lion Scratch")) ? "" : "Wins"}</h1>
-        <button onClick={clickHandler}>New Game</button>
+            <h1 className={winner ? 'result' : null}>{winner} {((winner === null) || (winner === "Lion Scratch")) ? "" : "Wins"}</h1>
+        <button className={winner ? 'pulse' : null} onClick={clickHandler}>New Game</button>
         </div>
     )
 }
+
+// {winner ? 'result' : null}
