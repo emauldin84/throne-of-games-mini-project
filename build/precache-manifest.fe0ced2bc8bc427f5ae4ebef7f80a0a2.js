e0ffe094405e4be90f21ff0899a0f86e591c9fbc
Margaret O'Neill <margaret@oneillfish.com>
2019-05-15 11:56:43 -0400

    new build

---
+++ build/precache-manifest.fe0ced2bc8bc427f5ae4ebef7f80a0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4762bf11118bffd839e300263c3c100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147474ce84cad0235808",
+    "url": "/static/css/main.feac86d9.chunk.css"
+  },
+  {
+    "revision": "f17f7a8c8d32ba0afc96",
+    "url": "/static/js/2.fd500808.chunk.js"
+  },
+  {
+    "revision": "147474ce84cad0235808",
+    "url": "/static/js/main.7222dea0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7694ba66cb30988c073f149d49adc9d",
+    "url": "/static/media/Game-of-Thrones-Iron-throne-640x447.e7694ba6.jpg"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/static/media/Game-of-Thrones.3af178ec.ttf"
+  }
+]);
